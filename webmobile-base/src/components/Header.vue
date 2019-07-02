222486ab1497d9801f5f0a3d96a87dcbfbcdd31e
jeong369 <injeong319@gmail.com>
2019-07-02 14:23:51 +0900

    사이드바 hidden 오류 display-none 해결

--- webmobile-base/src/components/Header.vue
+++ webmobile-base/src/components/Header.vue
@@ -8,26 +8,29 @@
       <v-btn flat href="/portfolio" class="hidden-sm-and-down">PORTFOLIO</v-btn>
       <v-btn flat href="/login" class="hidden-sm-and-down">LOGIN</v-btn>
       <!-- <v-btn flat><i class="material-icons hidden-md-and-up">menu</i></v-btn> -->
-      <v-menu bottom left>
-            <template v-slot:activator="{ on }">
-              <v-btn flat
-                icon
-                v-on="on"
-              >
-                <i class="material-icons hidden-md-and-up">menu</i>
-              </v-btn>
-            </template>
+      <div class="sidemenu mt-1">
+        <v-menu bottom left  class="sidemenu">
+              <template v-slot:activator="{ on }">
+                <v-btn flat
+                  icon
+                  v-on="on"
+                >
+                  <i class="material-icons">menu</i>
+                </v-btn>
+              </template>
 
-            <v-list>
-              <v-list-tile
-                v-for="(item, i) in items"
-                :key="i"
-                :href="item"
-              >
-                <v-list-tile-title>{{item}}</v-list-tile-title>
-              </v-list-tile>
-            </v-list>
-          </v-menu>
+              <v-list>
+                <v-list-tile
+                  v-for="(item, i) in items"
+                  :key="i"
+                  :href="item"
+                >
+                  <v-list-tile-title>{{item}}</v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+        </v-menu>
+      </div>
+
 
     </v-toolbar-items>
   </v-toolbar>
@@ -49,7 +52,7 @@
   }
 </script>
 <style>
-/* @media only screen and (min-width:600px){
-  .baricon {display:none;}
-} */
+@media (min-width:600px){
+  .sidemenu {display:none;}
+}
 </style>
